fa637b8e98b197f131547489915197bdd7e99552
White0ut <kdecline@gmail.com>
2014-04-05 18:18:49 -0400

    DB updates

--- ChurchLogger/src/com/wmu/churchlogger/DBAccess.java
+++ ChurchLogger/src/com/wmu/churchlogger/DBAccess.java
@@ -1,7 +1,42 @@
 package com.wmu.churchlogger;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
 public class DBAccess {
 	
-	public DBAccess(){}
+    String url = "jdbc:mysql://localhost:3306/";
+    String dbName = "CLogger";
+    String driver = "com.mysql.jdbc.Driver";
+    String userName = "pastor";
+    String password = "loggingrox";
+    
+    Connection conn;
+	
+	
+	public DBAccess(){
+		try {
+			
+			Class.forName(driver).newInstance();
+			conn = DriverManager.getConnection(url+dbName,userName,password);
+
+			System.out.println("Success");
+			
+		} catch (InstantiationException | IllegalAccessException
+				| ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			
+		}
+	}
 	
 }
