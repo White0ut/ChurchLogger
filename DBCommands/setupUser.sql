a763f5a05c015a99320baa10ac7b390e05b9ba76
White0ut <kdecline@gmail.com>
2014-04-05 18:52:10 -0400

    Function mysql DB

---
+++ DBCommands/setupUser.sql
@@ -0,0 +1,10 @@
+# This is used to initialize the pastor user for the Datase
+# login as root and source(setupUser.sql);
+
+GRANT USAGE ON *.* TO 'pastor'@127.0.0.1;
+
+DROP USER 'pastor'@127.0.0.1;
+
+CREATE USER 'pastor'@127.0.0.1 identified by 'loggingrox';
+
+GRANT ALL privileges on *.* to 'pastor'@127.0.0.1 IDENTIFIED BY 'loggingrox' WITH grant OPTION;
